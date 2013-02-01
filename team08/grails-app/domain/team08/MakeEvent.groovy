83e36c9ee5dbf529f86548dee448f40fa88d899e
b5310043 <rucussadoria@programmer.in.th>
2013-02-01 09:27:01 +0000

    b5310043 update

--- team08/grails-app/domain/team08/MakeEvent.groovy
+++ team08/grails-app/domain/team08/MakeEvent.groovy
@@ -3,6 +3,16 @@ package team08
 class MakeEvent {
 	NonMemberCustomer customer
 	Employee employee
+	String typere
+	String idre
+	String pricere
+	String statusre
     static constraints = {
+    	customer nullable:true
+    	employee nullable:true
+    	typere nullable:true
+    	idre nullable:true
+    	pricere nullable:true
+    	statusre nullable:true
     }
 }
