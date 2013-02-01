258a56f9b3755d7e34adace6c557a29b1da94fd1
b5308941 <giizbrellien@gmail.com>
2013-02-01 09:50:46 +0000

    update domain-class

--- team08/grails-app/domain/team08/MemberCustomer.groovy
+++ team08/grails-app/domain/team08/MemberCustomer.groovy
@@ -1,5 +1,16 @@
 package team08
 class MemberCustomer {
+
+	String fName
+	String mName
+	String lName
+	String gender
+	String age
+	String nationality
+	String idCityzen
+	String address
+	String emailAddreaa
+
     static constraints = {
     }
 }
