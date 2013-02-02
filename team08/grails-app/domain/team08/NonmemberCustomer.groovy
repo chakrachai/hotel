c36d470362c36445e6358c25d8936f0ee0f385fa
b5326716 <comma11558@hotmail.com>
2013-02-03 04:48:10 +0700

    Delete Domain Class Error

--- team08/grails-app/domain/team08/NonmemberCustomer.groovy
+++
@@ -1,7 +0,0 @@
-package team08
-
-class NonMemberCustomer {
-	static hasMany=[typeService:TypeService, monyService :MonyService , room:Room ,makeEvent:MakeEvent]
-    static constraints = {
-    }
-}
