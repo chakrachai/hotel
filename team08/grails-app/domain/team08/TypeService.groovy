9049ce00c4764c96d3a9e979b768cceeaf5748ba
b5302796 <mmknight.manetsu@gmail.com>
2013-02-04 02:57:15 +0700

    b5302796 commit

--- team08/grails-app/domain/team08/TypeService.groovy
+++ team08/grails-app/domain/team08/TypeService.groovy
@@ -3,7 +3,11 @@ package team08
 class TypeService {
 	NonMemberCustomer customer
 	Employee employee
-	
+	String servId
+	String servName
+	String servType
+	String servAmount
+	String servCost
     static constraints = {
     }
 }
