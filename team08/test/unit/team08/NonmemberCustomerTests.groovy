a9309524471a9f2a81b74277e086806b31845605
b5326716 <comma11558@hotmail.com>
2013-02-07 03:39:05 -0800

    edit error

--- team08/test/unit/team08/NonmemberCustomerTests.groovy
+++ team08/test/unit/team08/NonmemberCustomerTests.groovy
@@ -8,8 +8,8 @@ import org.junit.*
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(NonmemberCustomer)
-class NonmemberCustomerTests {
+@TestFor(NonMemberCustomer)
+class NonMemberCustomerTests {
 
     void testSomething() {
        fail "Implement me"
