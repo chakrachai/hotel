d6978919307238747fbc65b5866fd1f0d361c709
b5309030 <game_bigfoot@gmail.com>
2013-02-28 17:54:44 +0000

    config domain add zul

--- team08/grails-app/domain/team08/Employee.groovy
+++ team08/grails-app/domain/team08/Employee.groovy
@@ -1,11 +1,33 @@
 package team08
 
 class Employee {
-	String nameem
-	String classem
-	String idem
-	String statusem
+	String nameem			//ชื่อพนักงาน
+	String classem			//สาขางาน แม่บ้าน อื่นๆ
+	String idem				//รหัสพนักงาน
+	String statusem			//สถานะการทำงาน
+	String lnameem			//นามสกุลพนักงาน
+	String tel				//เบอร์โทร
+	String address			//ที่อยู่พนักงาน หอ บ้าน 
+	String numaddress		//เลขที่อยู่
+	String district			//อำเภอ
+	String subdistrict		//ตำบล
+	String province			//จังหวัด
+	String nprovince		//รหัสจังหวัด
+	String idppem			//รหัสประชาชนพนักงาน
 	static hasMany=[typeService:TypeService, monyService :MonyService , room:Room ,makeEvent:MakeEvent, timeWork:TimeWork]
     static constraints = {
+    	nameem nullable:true
+    	classem nullable:true
+    	idem nullable:true
+    	statusem nullable:true
+    	lnameem nullable:true
+    	tel nullable:true
+    	address nullable:true
+    	numaddress nullable:true
+    	district nullable:true
+    	subdistrict nullable:true
+    	province nullable:true
+    	nprovince nullable:true
+    	idppem nullable:true
     }
 }
