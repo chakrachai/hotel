a83d4541fa6328aecc3055b5164bf0b104938eb1
b5308941 <giizbrellien@gmail.com>
2013-02-28 10:30:25 -0800

    fixed #4256 Domain class for all

--- team08/grails-app/domain/team08/MemberCustomer.groovy
+++ team08/grails-app/domain/team08/MemberCustomer.groovy
@@ -1,19 +1,21 @@
 package team08
 class MemberCustomer {
 
-	String fName
-	String mName
-	String lName
-	String gender
-	String age
-	String nationality
-	String idCityzen
-	String address
-	String emailAddress
-	String memberId
-	String memberType
+	String fName			//ชื่อลูกค้า
+	String mName			//ชื่อกลาง
+	String lName			//นามสกุล
+	String gender			//เพศสภาพ
+	String born				//วันเกิด
+	String age				//อายุ
+	String nationality		//สัญชาติ
+	String idCityzen		//รหัสประจำตัวประชาชน
+	String address			//ที่อยู่ตามทะเบียนบ้าน
+	String emailAddress		//อีเมลล์ที่ติดต่อได้
+	String memberId			//รหัสลูกค้า
+	String memberType		//ประเภทลูกค้า[normal,VIP]
 
 
     static constraints = {
+    	mName nullable:true  //อนุญาติให้เป็นค่า ืีnull
     }
 }
