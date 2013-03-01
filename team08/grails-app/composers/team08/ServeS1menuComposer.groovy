818257945e4071ffb50a253b125dc6ca74af81a1
b5302796 <mmknight.manetsu@gmail.com>
2013-03-01 09:10:03 +0000

    fixed #5503 Create UI Prototype

---
+++ team08/grails-app/composers/team08/ServeS1menuComposer.groovy
@@ -0,0 +1,13 @@
+package team08
+
+import org.zkoss.zk.grails.composer.*
+
+import org.zkoss.zk.ui.select.annotation.Wire
+import org.zkoss.zk.ui.select.annotation.Listen
+
+class ServeS1menuComposer extends GrailsComposer {
+
+    def afterCompose = { window ->
+        // initialize components here
+    }
+}
