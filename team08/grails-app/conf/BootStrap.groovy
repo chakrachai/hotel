a7689b72ffbe64be01e60e21e198ccb71e44269c
B5310043 <rucussadoria@programmer.in.th>
2013-03-02 20:22:13 +0000

    fixed #4233 Improve UI

--- team08/grails-app/conf/BootStrap.groovy
+++ team08/grails-app/conf/BootStrap.groovy
@@ -8,6 +8,7 @@ class BootStrap {
 
 		def e1 = new Employee(nameem:"Employee1",classem:"serviceCustomer",idem:"E1234",statusem:"I Don't No",lnameem:"LEmployee",tel:"020000000",address:"Home",numaddress:"111",district:"Mairu",subdistrict:"lum",province:"Nakhonratchima",nprovince:"3000000",idppem:"1111111111",ageem:"30").save()
 
+		def re1 = new MakeEvent(employee:"Employee1",customer:"Customer1",typere:"Wedding Room",idre:"Mk001",pricere:"250000",statusre:"ON")
     }
     def destroy = {
     }
