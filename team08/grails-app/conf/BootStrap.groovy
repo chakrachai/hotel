59eeb4fd76efa23e47bda0ff2c1fe96e1fad267f
b5308941 <giizbrellien@gmail.com>
2013-03-03 11:10:39 +0000

    fixed #4265 Create Register UI

--- team08/grails-app/conf/BootStrap.groovy
+++ team08/grails-app/conf/BootStrap.groovy
@@ -6,7 +6,7 @@ class BootStrap {
 
     	def p1 = new MemberCustomer(fName:"Customer1",mName:"",lName:"LCustomer1",gender:"M",born:"Thai",age:"35",nationality:"Thai",idCityzen:"1111111111111",address:"OnTheRoad",emailAddress:"aaa@a.com",memberId:"C1234",memberType:"VIP").save()
 
-		def e1 = new Employee(nameem:"Employee1",classem:"serviceCustomer",idem:"E1234",statusem:"I Don't No",lnameem:"LEmployee",tel:"020000000",address:"Home",numaddress:"111",district:"Mairu",subdistrict:"lum",province:"Nakhonratchima",nprovince:"3000000",idppem:"1111111111",ageem:"30").save()
+		def e1 = new Employee(nameem:"Employee1",classem:"serviceCustomer",idem:"E1234",statusem:"I Don't No",lnameem:"LEmployee",tel:"020000000",address:"Home",numaddress:"111",district:"Mairu",subdistrict:"lum",province:"Nakhonratchima",nprovince:"3000000",idppem:"1111111111",ageem:"30",passem:"12234").save()
 
 		def re1 = new MakeEvent(employee:"Employee1",customer:"Customer1",typere:"Wedding Room",idre:"Mk001",pricere:"250000",statusre:"ON")
     }
