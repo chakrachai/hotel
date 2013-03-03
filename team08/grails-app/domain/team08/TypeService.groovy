bc2312be2eaed9008c32624be76a8eb988a93cd5
B5310043 <rucussadoria@programmer.in.th>
2013-03-03 21:50:54 +0000

    fixed #4249 fix domain service

--- team08/grails-app/domain/team08/TypeService.groovy
+++ team08/grails-app/domain/team08/TypeService.groovy
@@ -4,7 +4,6 @@ class TypeService {
 	Room room 					//ดึงเลขห้องออกมาระบุ
 	MemberCustomer customer 	//ดึงชื่อของลูกค้า
 	Employee employee 			//เอาไว้ดึงในfeatureสำหรับพนักงาน
-	String servId 				//ลำดับบริการ
 	String servType 			//ประเภทบริการ
 	String servName 			//ชื่อเมนู
 	String servCost 			//ราคา
