ad1445fd2e13642fd12d293a461ba3d97b578c19
b5302369 <nudream.sexy@gmail.com>
2013-03-04 18:15:58 +0000

    add zul

--- team08/grails-app/domain/team08/MonyService.groovy
+++ team08/grails-app/domain/team08/MonyService.groovy
@@ -3,10 +3,18 @@ package team08
 class MonyService {
 	MemberCustomer customer
 	Employee employee
+	String	dateIn
+	String	dateOut
 	String 	cradit
-	String	priceroom
+	String 	money
+	String	priceroom 
     static constraints = {
+		customer nullable:true
+		employee nullable:true
      	priceroom nullable:true
 		cradit nullable:true
+		money nullable:true
+		dateIn nullable:true
+		dateOut nullable:true
     }
 }
