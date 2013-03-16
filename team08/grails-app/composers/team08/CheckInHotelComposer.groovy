d9e724f1b25f3a7872f5b04a2f67b3fdb79c73a6
b5326716 <comma11558@hotmail.com>
2013-03-17 00:28:32 +0700

    New feature CheckinHotel

--- team08/grails-app/composers/team08/CheckInHotelComposer.groovy
+++ team08/grails-app/composers/team08/CheckInHotelComposer.groovy
@@ -13,6 +13,8 @@ class CheckInHotelComposer extends GrailsComposer {
         def datacustomer
         def customer
         def customernamedata =""
+        int sa
+        //def borrowday = new Date()borrowday.format("dd MMMM yyyy") 
         if (searchData.datainput==null || searchData.datainput==""){
             Executions.sendRedirect("/index.zul")
         }
@@ -24,48 +26,53 @@ class CheckInHotelComposer extends GrailsComposer {
             $('#employeeid')[0].text = employeedata
         }
 //=================================================checkdata==============================================================================
-   	$('#searchCustomer').on('click',{
-    		datacustomer = $('#customerId')[0].text
-    		customer = MemberCustomer.findByMemberId(datacustomer)
-    		if(customer == null){
-    			alert("ไม่พบข้อมูล")
-    		}else{
-    			$('#customerName')[0].text=customer.fName
-				$('#customerLastname')[0].text=customer.lName    			
-				$('#passport')[0].text=customer.idCityzen
+    $('#searchCustomer').on('click',{
+            datacustomer = $('#customerId')[0].text
+            customer = MemberCustomer.findByMemberId(datacustomer)
+            if(customer == null){
+                alert("ไม่พบข้อมูล")
+            }else{
+                $('#customerName')[0].text=customer.fName
+                $('#customerLastname')[0].text=customer.lName               
+                $('#passport')[0].text=customer.idCityzen
                 $('#searchCustomer').setVisible(false)
                 $('#customerName').setDisabled(true)
                 $('#customerLastname').setDisabled(true)
                 $('#passport').setDisabled(true)
                 $('#customerId').setDisabled(true)
-    		}
-    	})
+            }
+        })
 
 //==================================================find and show dataCustomer==========================================================
     $('#checkin').on('click',{
         if(datacustomer=="" | datacustomer==null){
          alert("ไม่มีข้อมูลลูกค้า")           
          }else{
+            if($('#customerMany')[0].text=="จำนวน"){
+                sa =1
+            }else{
+              sa = Integer.parseInt($('#customerMany')[0].text)
+            }
             def checkindata = new Room(
             roomNo : $('#room')[0].text,
             roomflore : $('#classRoom')[0].text,
-            dayIn : $('#dayin')[0].text,
-            dayOut : $('#dayout')[0].text,
+            dayIn : $('#dayin')[0].text+" "+$('#mountin')[0].text+" "+$('#yearin')[0].text,
+            dayOut : $('#dayout')[0].text+" "+$('#mountout')[0].text+" "+$('#yearout')[0].text,
             roomStatus: "ใช้งาน",
-            customermany:$('#customerMany')[0].text,
+            customermany:sa,
             customer:MemberCustomer.findByMemberId(datacustomer),
             employee:Employee.findByIdem(employeedata)
         ).save()
         $('#searchCustomer').setVisible(true)
         alert("save ห้องพักถูกใช้งาน")
-            $('#room')[0].text =""
-            $('#classRoom')[0].text =""
+            $('#room')[0].text ="ห้องที่"
+            $('#classRoom')[0].text ="ชั้นที่"
             $('#dayin')[0].text =""
-            $('#dayout')[0].text =""
+            $('#dayout')[0].text ="วัน"
             $('#customerName')[0].text =""
             $('#customerLastname')[0].text =""
             $('#passport')[0].text =""
-            $('#customerMany')[0].text =""
+            $('#customerMany')[0].text ="จำนวน"
             $('#customerId')[0].text =""
             $('#searchCustomer').setVisible(true)
             $('#customerName').setDisabled(true)
@@ -116,23 +123,25 @@ class CheckInHotelComposer extends GrailsComposer {
         $('#outroom').on('click',{
         if($('#roomList')!=null)
             $('#roomList > listitem').detach()
-        def checkindata = new Room(
+            def checkindata = new Room(
             roomNo : $('#room')[0].text,
             roomflore : $('#classRoom')[0].text,
-            dayIn : $('#dayin')[0].text,
-            dayOut : $('#dayout')[0].text,
+            dayIn : $('#dayin')[0].text+" "+$('#mountin')[0].text+" "+$('#yearin')[0].text,
+            dayOut : $('#dayout')[0].text+" "+$('#mountout')[0].text+" "+$('#yearout')[0].text,
             roomStatus: "ห้องชำรุด",
+            customermany:1,
+            customer:null,
             employee:Employee.findByIdem(employeedata)
         ).save()
         $('#searchCustomer').setVisible(true)
-            $('#room')[0].text =""
-            $('#classRoom')[0].text =""
+            $('#room')[0].text ="ห้องที่"
+            $('#classRoom')[0].text ="ชั้นที่"
             $('#dayin')[0].text =""
-            $('#dayout')[0].text =""
+            $('#dayout')[0].text ="วัน"
             $('#customerName')[0].text =""
             $('#customerLastname')[0].text =""
             $('#passport')[0].text =""
-            $('#customerMany')[0].text =""
+            $('#customerMany')[0].text ="จำนวน"
             $('#customerId')[0].text =""
             $('#customerLastname').setDisabled(false)
             $('#passport').setDisabled(false)
@@ -175,19 +184,20 @@ class CheckInHotelComposer extends GrailsComposer {
     })
 //=================================================================ปุ่มแจ้งซ้อม==============================================================
         $('#clearbtn').on('click',{
-            $('#room')[0].text =""
-            $('#classRoom')[0].text =""
+            $('#room')[0].text ="ห้องที่"
+            $('#classRoom')[0].text ="ชั้นที่"
             $('#dayin')[0].text =""
-            $('#dayout')[0].text =""
+            $('#dayout')[0].text ="วัน"
             $('#customerName')[0].text =""
             $('#customerLastname')[0].text =""
             $('#passport')[0].text =""
-            $('#customerMany')[0].text =""
+            $('#customerMany')[0].text ="จำนวน"
             $('#customerId')[0].text =""
             $('#searchCustomer').setVisible(true)
             $('#customerLastname').setDisabled(false)
             $('#passport').setDisabled(false)
             $('#customerId').setDisabled(false)
+
         })
 //====================================================================clearbutton============================================================   
        $('#reloadbtn').on('click',{
@@ -465,13 +475,256 @@ class CheckInHotelComposer extends GrailsComposer {
 //======================================================================================searchroom=================================================================
         $('#clearsearchdatabtn').on('click',{
             $('#searchstatus')[0].text = "จาก"
-            $('#tsearch')[0].text = ""
+            $('#tsearch')[0].text = ""            
         })
 //========================================================================================clearsearchroom==========================================================
         $('#btnsingout').on('click',{
             searchData.datainput = ""
             searchData.save()
             Executions.sendRedirect("/index.zul")
+        })
+//=====================================================================================signout=============================================
+        $('#classRoom').on('select',{
+                $('#room').append{
+                    for(int count=1;count<=30;count++)
+                    if(count<=9){
+                            comboitem(label:$('#classRoom')[0].text+"0"+count)
+                    }else{
+                            comboitem(label:$('#classRoom')[0].text+count)
+                    }
+             }
+        })
+//=====================================================================================selectClassfore===========================
+        $('#dayout').append{
+            for(int count=1;count<=31;count++){
+                comboitem(label:""+count)
+            }                        
+        }
+        $('#mountout').append{
+            comboitem(label:"มกราคม")
+            comboitem(label:"กุมภาพันธ์")
+            comboitem(label:"มีนาคม")
+            comboitem(label:"เมษายน")
+            comboitem(label:"พฤษภาคม")
+            comboitem(label:"มิถุนายน")
+            comboitem(label:"กรกฏาคม")
+            comboitem(label:"สิงหาคม")
+            comboitem(label:"กันยายน")
+            comboitem(label:"ตุลาคม")
+            comboitem(label:"พฤศจิกายน")
+            comboitem(label:"ธันวาคม")
+        }
+        $('#yearout').append{
+            for(int count=2550;count<=3000;count++){
+                comboitem(label:""+count)
+            }                        
+        }
+        $('#mountout').on('select',{
+            $('#dayout > comboitem').detach()
+            $('#mountout').append{
+            comboitem(label:"มกราคม")
+            comboitem(label:"กุมภาพันธ์")
+            comboitem(label:"มีนาคม")
+            comboitem(label:"เมษายน")
+            comboitem(label:"พฤษภาคม")
+            comboitem(label:"มิถุนายน")
+            comboitem(label:"กรกฏาคม")
+            comboitem(label:"สิงหาคม")
+            comboitem(label:"กันยายน")
+            comboitem(label:"ตุลาคม")
+            comboitem(label:"พฤศจิกายน")
+            comboitem(label:"ธันวาคม")
+        }
+        $('#yearout').append{
+            for(int count=2550;count<=3000;count++){
+                comboitem(label:""+count)
+            }                        
+        }
+            String s =$('#mountout')[0].text
+            if(s=="มกราคม" || s== "มีนาคม" || s== "พฤษภาคม" || s== "กรกฏาคม" || s== "สิงหาคม" || s== "ตุลาคม" || s== "ธันวาคม"){
+                $('#dayout').append{
+                    for(int count=1;count<=31;count++){
+                        comboitem(label:""+count)
+                    }                        
+                }
+                if($('#dayout')[0].text == "วัน"){
+                    $('#dayout')[0].text ="วัน"
+                }
+                else if(Integer.valueOf($('#dayout')[0].text)>=31){
+                    $('#dayout')[0].text ="31"
+                }else {
+ 
+                }
+            }else if (s=="เมษายน" || s== "มิถุนายน" || s== "กันยายน" || s== "พฤศจิกายน"){
+                $('#dayout').append{
+                    for(int count=1;count<=30;count++){
+                        comboitem(label:""+count)
+                    }                        
+                }
+                if($('#dayout')[0].text == "วัน"){
+                    $('#dayout')[0].text ="วัน"
+                }
+                else if(Integer.valueOf($('#dayout')[0].text)>=30){
+                    $('#dayout')[0].text ="30"
+                }else {
+ 
+                }
+            }else if(s=="กุมภาพันธ์"){
+                int time
+                if($('#yearout')[0].text=="ปี"){
+                   time=Integer.parseInt(new Date().format("yyyy"))
+                }else{
+                    time = Integer.parseInt($('#yearout')[0].text)-543    
+                }
+                if(time%4==0){
+                    $('#dayout').append{
+                        for(int count=1;count<=29;count++){
+                            comboitem(label:""+count)
+                        }                        
+                    }
+                        if($('#dayout')[0].text == "วัน"){
+                            $('#dayout')[0].text ="วัน"
+                        }
+                        else if(Integer.valueOf($('#dayout')[0].text)>=29){
+                            $('#dayout')[0].text ="29"
+                            alert("31")
+                        }else {
+         
+                        }
+                    }else{
+                        $('#dayout').append{
+                            for(int count=1;count<=28;count++){
+                                comboitem(label:""+count)
+                            }                        
+                        }
+                        if($('#dayout')[0].text == "วัน"){
+                            $('#dayout')[0].text ="วัน"
+                        }
+                        else if(Integer.valueOf($('#dayout')[0].text)>=28){
+                            $('#dayout')[0].text ="28"
+                        }else {
+         
+                        }
+                    }
+            }else{
+                alert("Error")
+            }
+        })
+//===============================================================================================outTime================================================================
+    $('#dayin').append{
+            for(int count=1;count<=31;count++){
+                comboitem(label:""+count)
+            }                        
+        }
+        $('#mountin').append{
+            comboitem(label:"มกราคม")
+            comboitem(label:"กุมภาพันธ์")
+            comboitem(label:"มีนาคม")
+            comboitem(label:"เมษายน")
+            comboitem(label:"พฤษภาคม")
+            comboitem(label:"มิถุนายน")
+            comboitem(label:"กรกฏาคม")
+            comboitem(label:"สิงหาคม")
+            comboitem(label:"กันยายน")
+            comboitem(label:"ตุลาคม")
+            comboitem(label:"พฤศจิกายน")
+            comboitem(label:"ธันวาคม")
+        }
+        $('#yearin').append{
+            for(int count=Integer.parseInt(new Date().format("yyyy"))+543;count<=3000;count++){
+                comboitem(label:""+count)
+            }                        
+        }
+        $('#mountin').on('select',{
+            $('#dayin > comboitem').detach()
+            $('#mountin').append{
+            comboitem(label:"มกราคม")
+            comboitem(label:"กุมภาพันธ์")
+            comboitem(label:"มีนาคม")
+            comboitem(label:"เมษายน")
+            comboitem(label:"พฤษภาคม")
+            comboitem(label:"มิถุนายน")
+            comboitem(label:"กรกฏาคม")
+            comboitem(label:"สิงหาคม")
+            comboitem(label:"กันยายน")
+            comboitem(label:"ตุลาคม")
+            comboitem(label:"พฤศจิกายน")
+            comboitem(label:"ธันวาคม")
+        }
+        $('#yearin').append{
+            for(int count=Integer.parseInt(new Date().format("yyyy"))+543;count<=3000;count++){
+                comboitem(label:""+count)
+            }                        
+        }
+            String s =$('#mountin')[0].text
+            if(s=="มกราคม" || s== "มีนาคม" || s== "พฤษภาคม" || s== "กรกฏาคม" || s== "สิงหาคม" || s== "ตุลาคม" || s== "ธันวาคม"){
+                $('#dayin').append{
+                    for(int count=1;count<=31;count++){
+                        comboitem(label:""+count)
+                    }                        
+                }
+                if($('#dayin')[0].text == "วัน"){
+                    $('#dayin')[0].text ="วัน"
+                }
+                else if(Integer.valueOf($('#dayin')[0].text)>=31){
+                    $('#dayin')[0].text ="31"
+                }else {
+ 
+                }
+            }else if (s=="เมษายน" || s== "มิถุนายน" || s== "กันยายน" || s== "พฤศจิกายน"){
+                $('#dayin').append{
+                    for(int count=1;count<=30;count++){
+                        comboitem(label:""+count)
+                    }                        
+                }
+                if($('#dayin')[0].text == "วัน"){
+                    $('#dayin')[0].text ="วัน"
+                }
+                else if(Integer.valueOf($('#dayin')[0].text)>=30){
+                    $('#dayin')[0].text ="30"
+                }else {
+ 
+                }
+            }else if(s=="กุมภาพันธ์"){
+                int time
+                if($('#yearin')[0].text=="ปี"){
+                   time=Integer.parseInt(new Date().format("yyyy"))
+                }else{
+                    time = Integer.parseInt($('#yearin')[0].text)-543    
+                }
+                if(time%4==0){
+                    $('#dayin').append{
+                        for(int count=1;count<=29;count++){
+                            comboitem(label:""+count)
+                        }                        
+                    }
+                        if($('#dayin')[0].text == "วัน"){
+                            $('#dayin')[0].text ="วัน"
+                        }
+                        else if(Integer.valueOf($('#dayin')[0].text)>=29){
+                            $('#dayin')[0].text ="29"
+                            alert("31")
+                        }else {
+         
+                        }
+                    }else{
+                        $('#dayin').append{
+                            for(int count=1;count<=28;count++){
+                                comboitem(label:""+count)
+                            }                        
+                        }
+                        if($('#dayin')[0].text == "วัน"){
+                            $('#dayin')[0].text ="วัน"
+                        }
+                        else if(Integer.valueOf($('#dayin')[0].text)>=28){
+                            $('#dayin')[0].text ="28"
+                        }else {
+         
+                        }
+                    }
+            }else{
+                alert("Error")
+            }
         })
     }
 }
--- team08/grails-app/domain/team08/Room.groovy
+++ team08/grails-app/domain/team08/Room.groovy
@@ -1,17 +1,17 @@
 package team08
 
 class Room {
-	MemberCustomer customer
-	Employee employee
-	String roomNo	//เลขห้อง
-	String roomflore //ชั้น
-	int customermany //จำนวนคน
-	String dayIn	//วันเข้า
-	String dayOut	//วันออก
-	String roomStatus	//สถานะห้อง
+    MemberCustomer customer
+    Employee employee
+    String roomNo   //เลขห้อง
+    String roomflore //ชั้น
+    int customermany //จำนวนคน
+    String dayIn    //วันเข้า
+    String dayOut   //วันออก
+    String roomStatus   //สถานะห้อง
     static constraints = {
-    	customer nullable:true
-    	roomStatus inList:['ว่าง','ไม่ว่าง','ห้องชำรุด']
-    	customermany range:1..4
+        customer nullable:true, blank: false
+        roomStatus inList:['ว่าง','ใช้งาน','ห้องชำรุด']
+        customermany range:1..4, blank: false
     }
 }
