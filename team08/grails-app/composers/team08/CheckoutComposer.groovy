b3940c70ba9841ce641134fba79a537dfa65baf4
b5302369 <nudream.sexy@gmail.com>
2013-03-18 17:28:26 +0100

    fixed #6000 addlase

--- team08/grails-app/composers/team08/CheckoutComposer.groovy
+++ team08/grails-app/composers/team08/CheckoutComposer.groovy
@@ -10,51 +10,72 @@ class CheckoutComposer extends GrailsComposer {
     def afterCompose = { window ->
 		def b
 		def e
-		String c 
-		int roommony = 15000
-		int	countmonny = 0	
+		def discount1
+		def payment1
+		//def checkmoneyevent
+		//def checkroom 
+		String c
+		int roommony = 20000
+		int	countmonny = 0
+		int paymoney
+
         $('#search').on('click',{
-		c=$('#idc')[0].getText()		
-		b=MemberCustomer.findByMemberId(c)
-		e=Room.findAllByCustomer(b)
-        	
-			$('#name')[0].setValue(b.fName+" "+b.lName)
+        	c=$('#idc')[0].getText()
+        	b=MemberCustomer.findByMemberId(c)
+        	e=Room.findAllByCustomer(b)
+        	//checkmoneyevent= MakeEvent.findAllByCustomer(b)
+
+        	$('#name')[0].setValue(b.fName+" "+b.lName)
 			$('#email')[0].setValue(b.emailAddress)
 			$('#tel')[0].setValue(b.telNo)
 			
-			//int daycount = 0
-			//for(loop in e){
-				$('#dateI').setValue(e.dayIn)
-				$('#dateO').setValue(e.dayOut)
-			//}						
-        $('#idc').setText("")
-    $('#service').append{
-        for(loopin in Room.findAllByCustomer(b)){
-        	if(loopin.roomStatus=="ว่าง"){
-        countmonny = countmonny+roommony
-        		listitem(value:loopin.id){
+        		int daycount = 0
+			if(e!=null){
+				for(loop in e){
+					$('#dateI').setValue(loop.dayIn)
+					$('#dateO').setValue(loop.dayOut)
+				}
+			}
+			$('#idc').setText(" ")
+        	
+        $('#service').append{	
+ 			for(loopin in Room.findAllByCustomer(b)){
+        		if(loopin.roomStatus=="ว่าง"){
+        		countmonny = countmonny+roommony
+        		  listitem(value:loopin.id){
                     listcell{   
                         label(value:loopin.roomNo)
                 	}
                 	listcell{   
                         label(value:"ค่าบริการ")
                 	}
-                	listcell{   
-                        label(value:roommony+"")
+                	listcell{  
+
+                        label(value:roommony)
+                        
                 	}
                 	listcell{   
-                        label(value:roommony+"")
+                        label(value:roommony)
                 	}
-            	}
-        	}
+            	  }
+        		}
+			}
 
-        }
-        }
-        	$('#money')[0].text = countmonny+""	
-        	$('#payment')[0].setValue(countmonny+"")
+    	}     
+    		$('#money')[0].text = countmonny+""	
+     		if(Integer.parseInt($('#money')[0].text) >= 5000){
+    				countmonny*=5/100
+    			paymoney = (Integer.parseInt($('#money')[0].text)-countmonny )
+    				discount1= String.valueOf(countmonny)
+    				payment1=String.valueOf(paymoney)
+    			$('#discount')[0].setValue(discount1)
+    			$('#payment')[0].setValue(payment1)
+    		}
 
+   	
 		})
-		
+
+	
 		
 		def money1=$('#money').getText()
 		
@@ -68,7 +89,7 @@ class CheckoutComposer extends GrailsComposer {
 				dateOut:e.dayOut,
 				money:money1,
 				cradit:cradit,
-				priceroom: "ooi"
+				priceroom:discount1 
 			)
 			savemoney.save()
 			for(loopin2 in e){
